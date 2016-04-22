c2cee67ff7231016e16aea7d41abb6839397e9b3
Jonathan Chang <jonathan.chang13@gmail.com>
2016-04-22 13:59:05 -0700

    Implemented tf-idf search system

--- tests.py
+++ tests.py
@@ -1,6 +1,14 @@
 import xkcdfinders
 import pandas as pd
 from collections import Counter
+from pathlib import Path
+from nltk.stem.porter import PorterStemmer
+from nltk.probability import FreqDist
+from nltk import word_tokenize
+import re
+
+# for a list of words seen in training data, what percentage of them do we want in the vocab?
+VOCAB_RATIO = 0.6
 
 topics = ["physics",
           "math",
@@ -66,7 +74,37 @@ def get_test_data(file_loc):
 
     return result
 
+def get_training_data(pathname):
+    result = []
+    for filename in pathname.glob('*'):
+        file_contents = ""
+        for line in filename.open():
+            file_contents += line
+        result.append(file_contents)
+    return result
+
+def build_vocab(training_data):
+    """
+    Construct a vocabulary from the given training data
+    """
+    stemmer = PorterStemmer()
+    words_encountered = FreqDist()
+    for text in training_data:
+        tokens = word_tokenize(text)
+        words_encountered.update([stemmer.stem(word.lower()) for word in tokens if re.search("[A-Za-z0-9]", word) is not None])
+    result = list(v[0] for v in words_encountered.most_common(int(words_encountered.B() * VOCAB_RATIO)))
+    # add the unknown word token
+    result.append("<UNK>")
+    return result
+
 if __name__ == '__main__':
     td = get_test_data("training/xkcd_responses.csv")
+    train = get_training_data(Path("data"))
+    vocab = build_vocab(train)
+    print("Testing baseline system...")
     baseline = xkcdfinders.TextualSearchXkcdFinder(debug=True)
     xkcdfinders.evaluate(baseline, topics, td)
+    print("Testing tf-idf system...")
+    tfidf = xkcdfinders.TfIdfXkcdFinder(vocab=vocab,debug=True)
+    tfidf.train(train)
+    xkcdfinders.evaluate(tfidf, topics, td)
--- xkcdfinders.py
+++ xkcdfinders.py
@@ -2,6 +2,8 @@ from abc import ABCMeta, abstractmethod
 from pyxkcd import get_all_xkcds
 from nltk.stem.porter import PorterStemmer
 from nltk import word_tokenize
+from nltk.probability import FreqDist
+import numpy, re
 
 class AbstractXkcdFinder(object):
     """
@@ -9,6 +11,10 @@ class AbstractXkcdFinder(object):
     """
 
     __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def train(self, training_data):
+        pass
 
     @abstractmethod
     def find(self, topic):
@@ -24,23 +30,32 @@ class TextualSearchXkcdFinder(AbstractXkcdFinder):
     system for performance comparisons, and should not be used in production.
     """
 
-    def __init__(self, debug=False):
+    def __init__(self, num_results=20, debug=False):
         self.stemmer = PorterStemmer()
+        self.num_results = num_results
         self.debug = debug
         self.xkcds = None
+
+    def train(self, training_data):
+        """
+        dummy training routine.
+        for a raw text search, no training is necessary.
+        """
+        return 1
 
     def find(self, topic):
         """
         Find all xkcds that directly mention the given topic.
         """
-        results = []
-        topic = topic.lower() # normalize to lowercase before doing comparisons
 
         # grab a list of all xkcds as strings. Use the cached version if one exists
         if not self.xkcds:
             if self.debug:
                 print("Retrieving xkcds, please wait...")
             self.xkcds = get_all_xkcds()
+
+        scores = numpy.zeros(len(self.xkcds))
+        topic = self.stemmer.stem(topic.lower()) # normalize to lowercase before doing comparisons
 
         for i in range(len(self.xkcds)):
             if self.debug:
@@ -49,12 +64,170 @@ class TextualSearchXkcdFinder(AbstractXkcdFinder):
             xkcd_text = self.xkcds[i]
 
             # look for the word inside the current xkcd, stemming words to allow for differences in tense
-            if self.stemmer.stem(topic) in [self.stemmer.stem(w.lower()) for w in word_tokenize(xkcd_text)]:
-                results.append(i+1)
+            for token in word_tokenize(xkcd_text):
+                if topic == self.stemmer.stem(token.lower()):
+                    scores[i] += 1
             if self.debug:
                 print('\n' if i+1 == len(self.xkcds) else '\r', end='')
 
-        return results
+        results = numpy.argpartition(-scores, self.num_results)[:self.num_results]
+        return results + 1
+
+class TfIdfXkcdFinder(AbstractXkcdFinder):
+    """
+    XkcdFinder implementation which computes an xkcd's relevance score by summing up
+    vector similarity between the input query and the string representation of the
+    xkcd. This implementation uses tf-idf weighted word-document matrix as its
+    vector space model.
+    """
+
+    def __init__(self, vocab=None, vocab_ratio=0.6, num_results=20, debug=False):
+        self.stemmer = PorterStemmer()
+        self.vocab = vocab
+        self.vocab_ratio = vocab_ratio
+        self.num_results = num_results
+        self.debug = debug
+        self.xkcds = None
+
+    def train(self, training_data):
+        """
+        Trains this finder's ti-idf model on the provided training data.
+        The training data is expected to come in the form of a list of strings.
+        """
+
+        # first, build a vocabulary if we were not given one
+        if not self.vocab:
+            if self.debug:
+                print("Building vocabulary, please wait...")
+            self.vocab = self._build_vocab(training_data)
+
+        # create a map from vocabulary words to indices in vocabulary, allowing constant-time
+        # conversion of words to indices in the vocabulary (allowing quicker lookup in the
+        # co-ocurrence matrix)
+        self.word_index = {}
+        for i in range(len(self.vocab)):
+            self.word_index[self.vocab[i]] = i
+
+        # grab a list of all xkcds as strings. Use the cached version if one exists
+        if not self.xkcds:
+            if self.debug:
+                print("Retrieving xkcds, please wait...")
+            self.xkcds = get_all_xkcds()
+
+        co_matrix = self._build_coocurrence_matrix(training_data)
+
+        # weight the co-ocurrence matrix using tf-idf weighting
+        if self.debug:
+            print("Performing ti-idf weighting, please wait...")
+        self.word_vectors = numpy.copy(co_matrix)
+        # first, fill the vector model with raw term frequencies
+        for td_idx in range(len(training_data)):
+            self.word_vectors[:,td_idx] = self.word_vectors[:,td_idx] / numpy.sum(self.word_vectors[:,td_idx])
+        # next, compute the idf for each word
+        idf_matrix = numpy.zeros(len(self.vocab))
+        for word_idx in range(len(self.vocab)):
+            for td_idx in range(len(training_data)):
+                if co_matrix[word_idx, td_idx] > 0:
+                    idf_matrix[word_idx] += 1
+        idf_matrix = numpy.log(len(training_data) / idf_matrix)
+        # finally, multiply to get the final ti-idf matrix
+        self.word_vectors *= idf_matrix.reshape(-1,1)
+
+        if self.debug:
+            print("Training completed")
+
+    def find(self, topic):
+        """
+        Find all xkcds with similar words to the given topic.
+        """
+        doc_vectors = numpy.zeros([len(self.word_vectors[0,:]), len(self.xkcds)])
+        scores = numpy.zeros(len(self.xkcds))
+        topic = self.stemmer.stem(topic.lower())
+
+        for i in range(len(self.xkcds)):
+            if self.debug:
+                print("Searching xkcd {} of {}".format(i+1, len(self.xkcds)), end='')
+
+            xkcd_text = self.xkcds[i]
+            words_considered = 0
+
+            # compute similarities for each word in the text of the current xkcd
+            for token in word_tokenize(xkcd_text):
+                if re.search("[A-Za-z0-9]", token) is not None:
+                    cur_word = self.stemmer.stem(token.lower())
+                    if cur_word not in self.word_index:
+                        cur_word = "<UNK>"
+                    #scores[i] += self._stemmed_similarity(topic, cur_word)
+                    doc_vectors[:,i] += self.word_vectors[self.word_index[cur_word],:]
+                    words_considered += 1
+
+            # to avoid wrongly preferring longer xkcds, we normalize by document length
+            #scores[i] /= words_considered
+
+            if self.debug:
+                print('\n' if i+1 == len(self.xkcds) else '\r', end='')
+
+        topic_vec = self.word_vectors[self.word_index[topic],:]
+
+        for i in range(len(self.xkcds)):
+            scores[i] = topic_vec.dot(doc_vectors[:,i]) / (numpy.linalg.norm(topic_vec) * numpy.linalg.norm(doc_vectors[:,i]))
+        results = numpy.argpartition(-scores, self.num_results)[:self.num_results]
+        return results + 1
+
+    def similarity(self, word1, word2):
+        """
+        Uses the trained vector space model to compute similarity between two words
+        """
+        word1 = self.stemmer.stem(word1.lower())
+        word2 = self.stemmer.stem(word2.lower())
+        if word1 not in self.word_index:
+            print("Warning: unrecognized word {}".format(word1))
+            word1 = "<UNK>"
+        if word2 not in self.word_index:
+            print("Warning: unrecognized word {}".format(word2))
+            word2 = "<UNK>"
+        return self._stemmed_similarity(word1, word2)
+
+    def _stemmed_similarity(self, word1, word2):
+        index1 = self.word_index[word1]
+        index2 = self.word_index[word2]
+        vector1 = self.word_vectors[index1,:]
+        vector2 = self.word_vectors[index2,:]
+        return vector1.dot(vector2) / (numpy.linalg.norm(vector1) * numpy.linalg.norm(vector2))
+
+    def _build_vocab(self, training_data):
+        """
+        Construct a vocabulary from the given training data
+        """
+        words_encountered = FreqDist()
+        for text in training_data:
+            tokens = word_tokenize(text)
+            words_encountered.update([self.stemmer.stem(word.lower()) for word in tokens if re.search("[A-Za-z0-9]", word) is not None])
+        result = list(v[0] for v in words_encountered.most_common(int(words_encountered.B() * self.vocab_ratio)))
+        # add the unknown word token
+        result.append("<UNK>")
+        return result
+
+    def _build_coocurrence_matrix(self, training_data):
+        """
+        build a word-document co-ocurrence matrix with the given training data.
+        """
+        if self.debug:
+            print("Building co-occurence matrix, please wait...")
+        # co-occurence matrix is indexed by (word,document)
+        co_matrix = numpy.zeros([len(self.vocab), len(training_data)])
+        for td_idx in range(len(training_data)):
+            text = training_data[td_idx]
+            tokens = word_tokenize(text)
+            for token in tokens:
+                clean_token = self.stemmer.stem(token.lower())
+                if clean_token not in self.word_index:
+                    clean_token = "<UNK>"
+                # only consider tokens that have some alphanumeric characters to be real words
+                if re.search("[A-Za-z0-9]", clean_token) is not None:
+                    word_idx = self.word_index[clean_token]
+                    co_matrix[word_idx, td_idx] += 1
+        return co_matrix
 
 def evaluate(finder, searches, labeled_data):
     """
